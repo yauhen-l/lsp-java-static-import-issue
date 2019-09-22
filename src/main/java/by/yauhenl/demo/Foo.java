1469c1aed3cd3bcfc7199d0c9bcdaf764225d056
Yauhen Lazurkin <yauhen.lazurkin@deposit-solutions.com>
2019-09-22 21:35:05 +0200

    init repo

---
+++ src/main/java/by/yauhenl/demo/Foo.java
@@ -0,0 +1,9 @@
+package by.yauhenl.demo;
+
+public class Foo {
+
+	public static int bar() {
+		return 1;
+	}
+
+}
---
+++ src/main/java/by/yauhenl/demo/meow/Meow.java
@@ -0,0 +1,12 @@
+package by.yauhenl.demo.meow;
+
+import by.yauhenl.demo.Foo;
+
+public class Meow {
+
+		public static int meow() {
+				Foo f = new Foo();
+				return Foo.bar();
+		}
+
+}
